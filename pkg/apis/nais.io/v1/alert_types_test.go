e351b1b3d243c1da256a1dda34d58b3dad676540
Kyrre Havik <kyrremann@gmail.com>
2021-01-26 10:05:03 +0100

    fix: fjernet tester som ikke brukes lengre

--- pkg/apis/nais.io/v1/alert_types_test.go
+++ pkg/apis/nais.io/v1/alert_types_test.go
@@ -25,32 +25,3 @@ func TestNilFix(t *testing.T) {
 	assert.NotNil(t, alert.Spec.Receivers)
 	assert.NotNil(t, alert.Spec.Alerts)
 }
-
-func TestAlertRulesValidationWithEmptyForValue(t *testing.T) {
-	alert := GenerateAlertWithForValue("")
-	err := alert.ValidateAlertFields()
-	assert.Error(t, err)
-}
-
-func TestAlertRulesValidationWithValidForValue(t *testing.T) {
-	alert := GenerateAlertWithForValue("1m")
-	err := alert.ValidateAlertFields()
-	assert.NoError(t, err)
-}
-
-func TestAlertRulersValidationWithInvalidForValue(t *testing.T) {
-	alert := GenerateAlertWithForValue("foo")
-	err := alert.ValidateAlertFields()
-	assert.Error(t, err)
-}
-
-func GenerateAlertWithForValue(forValue string) nais_io_v1.Alert {
-	return nais_io_v1.Alert{Spec: nais_io_v1.AlertSpec{Alerts: []nais_io_v1.Rule{
-		{
-			Alert:  "app is down",
-			For:    forValue,
-			Expr:   "kube_deployment_status_replicas_unavailable{deployment=\"my-app\"} > 0",
-			Action: "kubectl describe pod -l app=my-app",
-		},
-	}}}
-}
