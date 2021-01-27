aec5280b4cd32de89699bca9cda3ee97dd8e144f
Kim Tore Jensen <kimtjen@gmail.com>
2021-01-27 10:18:52 +0100

    import nais.io types from naiserator

--- go.mod
+++ go.mod
@@ -3,8 +3,11 @@ module github.com/nais/liberator
 go 1.15
 
 require (
+	github.com/google/uuid v1.1.1
+	github.com/imdario/mergo v0.3.6
 	github.com/mitchellh/hashstructure v1.1.0
 	github.com/mitchellh/mapstructure v1.1.2
+	github.com/sirupsen/logrus v1.4.2
 	github.com/spf13/pflag v1.0.5
 	github.com/spf13/viper v1.3.2
 	github.com/stretchr/testify v1.6.1
---
+++ pkg/stringutil/strings.go
@@ -0,0 +1,41 @@
+package stringutil
+
+import (
+	"math"
+	"math/rand"
+	"time"
+)
+
+const strTrimMiddleTruncate = "---[truncated]---"
+const strTrimRightTruncate = "..."
+
+func StrTrimRight(s string, maxlen int) string {
+	l := len(strTrimRightTruncate)
+	if maxlen > l {
+		return s[:maxlen-l] + strTrimRightTruncate
+	}
+	return s[:maxlen]
+}
+
+func StrTrimMiddle(s string, maxlen int) string {
+	if len(s) <= maxlen {
+		return s
+	}
+	newlen := maxlen - len(strTrimMiddleTruncate)
+	if newlen < len(strTrimMiddleTruncate) {
+		return StrTrimRight(s, maxlen)
+	}
+	partlen := int(math.Floor(float64(newlen)) / 2)
+	return s[:partlen] + strTrimMiddleTruncate + s[len(s)-partlen:]
+}
+
+func RandomString(length int) string {
+	const charset = "abcdefghijklmnopqrstuvwxyz0123456789"
+	b := make([]byte, length)
+	seededRand := rand.New(rand.NewSource(time.Now().UnixNano()))
+
+	for i := range b {
+		b[i] = charset[seededRand.Intn(len(charset))]
+	}
+	return string(b)
+}
