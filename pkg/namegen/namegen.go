cbd989bc82628e50ed68aa584c7ed54c8452cf4c
Trong Huu Nguyen <trong.huu.nguyen@nav.no>
2021-03-14 20:16:41 +0100

    namegen: add shortname generator with random suffix and optional prefix

--- pkg/namegen/namegen.go
+++ pkg/namegen/namegen.go
@@ -2,6 +2,7 @@ package namegen
 
 import (
 	"fmt"
+	"github.com/nais/liberator/pkg/keygen"
 	"hash/crc32"
 )
 
@@ -21,4 +22,18 @@ func ShortName(basename string, maxlen int) (string, error) {
 		basename = basename[:maxlen]
 	}
 	return basename + "-" + hashStr, nil
+}
+
+func RandShortName(basename string, maxlen int) string {
+	randlen := 8
+	maxlen -= randlen + 1 // length of random bytes and 1 byte of separator
+
+	if len(basename) > maxlen {
+		basename = basename[:maxlen]
+	}
+	return fmt.Sprintf("%s-%s", basename, keygen.RandStringBytes(randlen))
+}
+
+func PrefixedRandShortName(prefix, basename string, maxlen int) string {
+	return RandShortName(fmt.Sprintf("%s-%s", prefix, basename), maxlen)
 }
