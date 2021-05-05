6caf18858e8ad73d8bc4451810100772bad372fe
Morten Lied Johansen <morten.lied.johansen@nav.no>
2021-05-05 21:11:40 +0200

    Make AivenApplicationStatus more informative

--- pkg/apis/kafka.nais.io/v1/aiven_application_types.go
+++ pkg/apis/kafka.nais.io/v1/aiven_application_types.go
@@ -1,7 +1,9 @@
 package kafka_nais_io_v1
 
 import (
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"time"
 )
 
 func init() {
@@ -20,6 +22,7 @@ type AivenApplicationList struct {
 
 // +kubebuilder:object:root=true
 // +kubebuilder:resource:shortName={"aivenapp"}
+// +kubebuilder:subresource:status
 // +kubebuilder:printcolumn:name="Name of secret",type=string,JSONPath=".spec.secretName"
 // +kubebuilder:printcolumn:name="State",type="string",JSONPath=".status.synchronizationState",priority=10
 // +kubebuilder:printcolumn:name="Synced",type="date",JSONPath=".status.synchronizationTime",priority=20
@@ -43,9 +46,30 @@ type KafkaSpec struct {
 	Pool string `json:"pool"`
 }
 
-// +kubebuilder:subresource:status
+type AivenApplicationConditionType string
+
+const (
+	AivenApplicationSucceeded    AivenApplicationConditionType = "Succeeded"
+	AivenApplicationAivenFailure AivenApplicationConditionType = "AivenFailure"
+	AivenApplicationLocalFailure AivenApplicationConditionType = "LocalFailure"
+)
+
+// AivenApplicationCondition describes the state of a deployment at a certain point.
+type AivenApplicationCondition struct {
+	// Type of condition.
+	Type AivenApplicationConditionType `json:"type"`
+	// Status of the condition, one of True, False, Unknown.
+	Status corev1.ConditionStatus `json:"status"`
+	// The last time this condition was updated.
+	LastUpdateTime metav1.Time `json:"lastUpdateTime,omitempty"`
+	// The reason for the condition's last transition.
+	Reason string `json:"reason,omitempty"`
+	// A human readable message indicating details about the transition.
+	Message string `json:"message,omitempty"`
+}
+
 type AivenApplicationStatus struct {
-	// SynchronizationHash is the hash of the AivenApplication object
+	// SynchronizationHash is the hash of the AivenApplication object most recently successfully synchronized
 	SynchronizationHash string `json:"synchronizationHash,omitempty"`
 	// SynchronizationSecretName is the SecretName set in the last successful synchronization
 	SynchronizationSecretName string `json:"synchronizationSecretName,omitempty"`
@@ -53,6 +77,12 @@ type AivenApplicationStatus struct {
 	SynchronizationState string `json:"synchronizationState,omitempty"`
 	// SynchronizationTime is the last time the Status subresource was updated
 	SynchronizationTime *metav1.Time `json:"synchronizationTime,omitempty"`
+	// SynchronizedGeneration is the generation most recently successfully synchronized by Aivenator
+	SynchronizedGeneration int64 `json:"synchronizedGeneration,omitempty"`
+	// ObservedGeneration is the generation most recently observed by Aivenator
+	ObservedGeneration int64 `json:"observedGeneration,omitempty"`
+	// Represents the latest available observations of an AivenApplications' current state.
+	Conditions []AivenApplicationCondition `json:"conditions,omitempty"`
 }
 
 func (in *AivenApplication) GetOwnerReference() metav1.OwnerReference {
@@ -62,4 +92,16 @@ func (in *AivenApplication) GetOwnerReference() metav1.OwnerReference {
 		Name:       in.Name,
 		UID:        in.UID,
 	}
+}
+
+func (in *AivenApplicationStatus) AddCondition(condition AivenApplicationCondition) {
+	condition.LastUpdateTime = metav1.Time{time.Now()}
+	conditions := make([]AivenApplicationCondition, 0, len(in.Conditions))
+	for _, c := range in.Conditions {
+		if c.Type != condition.Type {
+			conditions = append(conditions, c)
+		}
+	}
+	conditions = append(conditions, condition)
+	in.Conditions = conditions
 }
