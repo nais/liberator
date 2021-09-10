6ccadcba7fef13555ab1d119dcb5684ba611abe9
thokra-nav <85170275+thokra-nav@users.noreply.github.com>
2021-09-10 15:35:38 +0200

    Generate and upload json-spec for nais and k8s resources. (#33)
    * Generate and upload json-spec for nais and k8s resources.
    
    * Update workflow and pin gsutil

---
+++ hack/build-json-spec.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+set -e
+
+K8S_VERSION=v1.19.12
+
+OPENAPI_DIR=$(pwd)/doc/output/openapi
+
+# Heavily inspired by https://github.com/yannh/kubernetes-json-schema/blob/master/build.sh
+OPENAPI2JSONSCHEMABIN="docker create -i ghcr.io/yannh/openapi2jsonschema:latest"
+
+# Generate json spec for k8s resources
+container=$($OPENAPI2JSONSCHEMABIN -o "schemas/kubernetes" --kubernetes --stand-alone --expanded --strict "https://raw.githubusercontent.com/kubernetes/kubernetes/${K8S_VERSION}/api/openapi-spec/swagger.json")
+set +e
+docker start -ai "$container"
+set -e
+rm -rf "$OPENAPI_DIR/kubernetes"
+docker cp "$container:/out/schemas/kubernetes" "$OPENAPI_DIR"
+docker rm "$container"
+
+# Make a json file with all nais resources
+echo '{"oneOf":[' > "$OPENAPI_DIR/nais-all.json"
+list=$(ls "$OPENAPI_DIR/nais/")
+mapfile -t FILES <<< "$list"
+for file in ${FILES[*]}; do
+	echo "{\"\$ref\":\"nais/$(basename "$file")\"}," >> "$OPENAPI_DIR/nais-all.json"
+done
+truncate -s-2 "$OPENAPI_DIR/nais-all.json"
+echo "]}" >> "$OPENAPI_DIR/nais-all.json"
+
+# Combine nais and k8s resources, but only those with an enum defined for the kind (otherwise there's completion problems)
+jq_k8s='
+	{
+		"oneOf":
+		(
+			.[1].oneOf +
+				(
+					.[0].definitions
+					| to_entries
+					| map(select(.value.properties.kind.enum))
+					| map({"$ref": ("kubernetes/_definitions.json#/definitions/"+ .key)})
+				)
+		)
+	}
+'
+jq -s "$jq_k8s" "$OPENAPI_DIR/kubernetes/_definitions.json" "$OPENAPI_DIR/nais-all.json" > \
+		"$OPENAPI_DIR/nais-k8s-all.json"
