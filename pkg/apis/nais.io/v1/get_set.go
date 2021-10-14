256a31ce9b9cae29007da2f6fa6ddcaaacef1dda
Kim Tore Jensen <kimtjen@gmail.com>
2021-10-14 11:03:26 +0200

    change type and getter name for naisjob concurrencypolicy

--- pkg/apis/nais.io/v1/get_set.go
+++ pkg/apis/nais.io/v1/get_set.go
@@ -1,9 +1,22 @@
 package nais_io_v1
 
+import (
+	"k8s.io/api/batch/v1beta1"
+)
+
 func (in *Naisjob) GetStatus() *Status {
 	return &in.Status
 }
 
 func (in *Naisjob) SetStatus(status *Status) {
 	in.Status = *status
+}
+
+func (in *Naisjob) GetConcurrencyPolicy() string {
+	switch in.Spec.ConcurrencyPolicy {
+	case string(v1beta1.ForbidConcurrent), string(v1beta1.ReplaceConcurrent):
+		return in.Spec.ConcurrencyPolicy
+	default:
+		return string(v1beta1.AllowConcurrent)
+	}
 }
