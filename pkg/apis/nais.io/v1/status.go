c780c6d0d2bfaabf88ab9544db4abea87c2a2aa2
Kim Tore Jensen <kimtjen@gmail.com>
2021-10-18 10:40:57 +0200

    Extend status struct with conditions (#39)
    Co-authored-by: Vignesh Sivarajah <47299342+VigneshSivar@users.noreply.github.com>
    Co-authored-by: Bjarte Karlsen <bjarte.karlsen@skatteetaten.no>

--- pkg/apis/nais.io/v1/status.go
+++ pkg/apis/nais.io/v1/status.go
@@ -1,11 +1,72 @@
 package nais_io_v1
 
+import (
+	"time"
+
+	"k8s.io/apimachinery/pkg/api/meta"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+const (
+	EventSynchronized          = "Synchronized"
+	EventRolloutComplete       = "RolloutComplete"
+	EventFailedPrepare         = "FailedPrepare"
+	EventFailedSynchronization = "FailedSynchronization"
+	EventFailedStatusUpdate    = "FailedStatusUpdate"
+	EventRetrying              = "Retrying"
+)
+
 // Status contains different NAIS status properties
 type Status struct {
-	SynchronizationTime     int64  `json:"synchronizationTime,omitempty"`
-	RolloutCompleteTime     int64  `json:"rolloutCompleteTime,omitempty"`
-	CorrelationID           string `json:"correlationID,omitempty"`
-	DeploymentRolloutStatus string `json:"deploymentRolloutStatus,omitempty"`
-	SynchronizationState    string `json:"synchronizationState,omitempty"`
-	SynchronizationHash     string `json:"synchronizationHash,omitempty"`
+	SynchronizationTime     int64               `json:"synchronizationTime,omitempty"`
+	RolloutCompleteTime     int64               `json:"rolloutCompleteTime,omitempty"`
+	CorrelationID           string              `json:"correlationID,omitempty"`
+	DeploymentRolloutStatus string              `json:"deploymentRolloutStatus,omitempty"`
+	SynchronizationState    string              `json:"synchronizationState,omitempty"`
+	SynchronizationHash     string              `json:"synchronizationHash,omitempty"`
+	Conditions              *[]metav1.Condition `json:"conditions,omitempty"`
+}
+
+func (in *Status) SetStatusConditions() {
+
+	if in.Conditions == nil {
+		in.Conditions = &[]metav1.Condition{}
+	}
+
+	reconcilingConditionStatus := metav1.ConditionFalse
+	if in.SynchronizationState != EventRolloutComplete && in.SynchronizationState != EventFailedSynchronization {
+		reconcilingConditionStatus = metav1.ConditionTrue
+	}
+	readyConditionStatus := metav1.ConditionFalse
+	if in.SynchronizationState == EventRolloutComplete {
+		readyConditionStatus = metav1.ConditionTrue
+	}
+
+	stalledConditionStatus := metav1.ConditionFalse
+	if in.SynchronizationState == EventFailedSynchronization {
+		stalledConditionStatus = metav1.ConditionTrue
+	}
+
+	in.addStatusCondition("Ready", readyConditionStatus)
+	in.addStatusCondition("Stalled", stalledConditionStatus)
+	in.addStatusCondition("Reconciling", reconcilingConditionStatus)
+}
+
+func (in *Status) addStatusCondition(name string, conditionStatus metav1.ConditionStatus) {
+	condition := meta.FindStatusCondition(*in.Conditions, name)
+	if condition == nil {
+		condition = &metav1.Condition{
+			Type:               name,
+			Status:             conditionStatus,
+			LastTransitionTime: metav1.NewTime(time.Now()),
+			Reason:             in.SynchronizationState,
+			Message:            in.DeploymentRolloutStatus,
+		}
+	} else {
+		condition.Status = conditionStatus
+		condition.Reason = in.SynchronizationState
+		condition.Message = in.DeploymentRolloutStatus
+	}
+
+	meta.SetStatusCondition(in.Conditions, *condition)
 }
