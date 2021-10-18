90009eda1e273fcb11c2d79aa67cee755d338c26
Kim Tore Jensen <kimtjen@gmail.com>
2021-10-18 13:39:16 +0200

    add missing getters

--- pkg/apis/nais.io/v1/get_set.go
+++ pkg/apis/nais.io/v1/get_set.go
@@ -4,6 +4,16 @@ import (
 	"k8s.io/api/batch/v1beta1"
 )
 
+// +kubebuilder:object:generate=false
+type AzureInterface interface {
+	GetApplication() *AzureApplication
+	GetSidecar() *AzureSidecar
+}
+
+func (in *Naisjob) GetPort() int {
+	return 0
+}
+
 func (in *Naisjob) GetStatus() *Status {
 	return &in.Status
 }
@@ -23,4 +33,32 @@ func (in *Naisjob) GetConcurrencyPolicy() string {
 	default:
 		return string(v1beta1.AllowConcurrent)
 	}
+}
+
+func (in *Naisjob) GetAccessPolicy() *AccessPolicy {
+	return in.Spec.AccessPolicy
+}
+
+func (in *Naisjob) GetAzure() AzureInterface {
+	return in.Spec.Azure
+}
+
+func (in *Naisjob) GetIngress() []Ingress {
+	return nil
+}
+
+func (in *Azure) GetApplication() *AzureApplication {
+	return in.Application
+}
+
+func (in *Azure) GetSidecar() *AzureSidecar {
+	return in.Sidecar
+}
+
+func (in *AzureNaisJob) GetApplication() *AzureApplication {
+	return in.Application
+}
+
+func (in *AzureNaisJob) GetSidecar() *AzureSidecar {
+	return nil
 }
