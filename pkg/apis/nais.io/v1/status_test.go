a8d72e50bc1e19e51fb3f2b45c9515ad78072b90
Kyrre Havik <kyrremann@gmail.com>
2022-03-23 09:34:10 +0100

    fix: feil variabelnavn i test

--- pkg/apis/nais.io/v1/status_test.go
+++ pkg/apis/nais.io/v1/status_test.go
@@ -34,7 +34,7 @@ func TestGenerateCorrectKStatus(t *testing.T) {
 		{v1.EventFailedStatusUpdate, []string{"Reconciling"}},
 		{v1.EventRetrying, []string{"Reconciling"}},
 		{v1.EventFailedPrepare, []string{"Reconciling"}},
-		{v1.EventJobComplete, []string{"Ready"}},
+		{v1.EventJobCompleted, []string{"Ready"}},
 		{v1.EventJobFailed, []string{"Stalled"}},
 	}
 
