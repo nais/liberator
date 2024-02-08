a4ddc1dd2ffa187793979c793cc92bfa57bcb0f6
Kim Tore Jensen <kimtjen@gmail.com>
2024-02-08 12:47:03 +0100

    Documentation for synchronization states, and add FailedGenerate

--- pkg/events/events.go
+++ pkg/events/events.go
@@ -1,11 +1,27 @@
 package events
 
-// Machine readable status strings describing the current synchronization state of a custom resource.
+// Machine readable status strings describing the current synchronization state of a NAIS application or job.
+// These may be used both in the `.status.synchronizationState` and `.status.conditions[].reason` fields.
+
 const (
-	FailedPrepare         = "FailedPrepare"
-	FailedStatusUpdate    = "FailedStatusUpdate"
+	// Unable to read preliminary data from cluster. This error is always transient.
+	FailedPrepare = "FailedPrepare"
+
+	// Indicates invalid configuration in nais.yaml.
+	FailedGenerate = "FailedGenerate"
+
+	// Naiserator failed to persist resources into the cluster, and the error is permanent.
 	FailedSynchronization = "FailedSynchronization"
-	Retrying              = "Retrying"
-	RolloutComplete       = "RolloutComplete"
-	Synchronized          = "Synchronized"
+
+	// Naiserator failed to persist resources into the cluster, and the error is transient.
+	Retrying = "Retrying"
+
+	// Naiserator has persisted resources into the cluster, and the rollout is in progress.
+	Synchronized = "Synchronized"
+
+	// Indicates that everything is complete - the most recently deployed version of the application has been activated.
+	RolloutComplete = "RolloutComplete"
+
+	// Emitted by events when the status field cannot be updated.
+	FailedStatusUpdate = "FailedStatusUpdate"
 )
