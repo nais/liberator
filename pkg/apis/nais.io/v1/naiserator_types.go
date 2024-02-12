45987246fc06d31e77c48b0d8903e13c9c28bc6c
Johnny Horvi <johnny@horvi.no>
2024-02-12 09:27:57 +0100

    Remove references to sizing postgres
    Not able to find it, we tried very hard - the three of us

--- pkg/apis/nais.io/v1/naiserator_types.go
+++ pkg/apis/nais.io/v1/naiserator_types.go
@@ -530,7 +530,6 @@ type CloudSqlInstance struct {
 	// Server tier, i.e. how much CPU and memory allocated.
 	// Available tiers are `db-f1-micro`, `db-g1-small` and custom `db-custom-CPU-RAM`.
 	// Custom memory must be mulitple of 256 MB and at least 3.75 GB (e.g. `db-custom-1-3840` for 1 cpu, 3840 MB ram)
-	// Also check out [sizing your database](../../persistence/postgres/#sizing-your-database).
 	// +kubebuilder:validation:Pattern="db-.+"
 	// +nais:doc:Default="db-f1-micro"
 	Tier string `json:"tier,omitempty"`
