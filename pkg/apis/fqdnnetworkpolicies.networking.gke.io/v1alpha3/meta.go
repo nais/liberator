8f98df7260982e9622091ab78ddfcef7c3ed7f02
Kyrre Havik <kyrre.havik@nav.no>
2025-02-19 14:53:08 +0100

    fix: feil group for fqdn crd
    Co-authored-by: frodesundby <frode.sundby@nav.no>
    Co-authored-by: Carl Hedgren <carl.hedgren@nav.no>
    Co-authored-by: Morten Lied Johansen <morten.lied.johansen@nav.no>
    Co-authored-by: Sten Røkke <sten.ivar.rokke@nav.no>

--- pkg/apis/fqdnnetworkpolicies.networking.gke.io/v1alpha3/meta.go
+++ pkg/apis/fqdnnetworkpolicies.networking.gke.io/v1alpha3/meta.go
@@ -1,5 +1,5 @@
 // +kubebuilder:object:generate=true
-// +groupName=fqdnnetworkpolicies.networking.gke.io
+// +groupName=networking.gke.io
 // +versionName=v1alpha3
 package fqdnnetworkpolicies_networking_gke_io_v1alpha3
 
@@ -10,7 +10,7 @@ import (
 
 var (
 	// GroupVersion is group version used to register these objects
-	GroupVersion = schema.GroupVersion{Group: "fqdnnetworkpolicies.networking.gke.io", Version: "v1alpha3"}
+	GroupVersion = schema.GroupVersion{Group: "networking.gke.io", Version: "v1alpha3"}
 
 	// SchemeBuilder is used to add go types to the GroupVersionKind scheme
 	SchemeBuilder = &scheme.Builder{GroupVersion: GroupVersion}
