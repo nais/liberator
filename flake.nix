8b0d8b72819c1db850423b7a4b75eeb9e5f413ac
@x10an14-nav <christian.chavez@nav.no>
2025-07-09 12:51:43 +0200

    flake: Update inputs + automatically follow `go.mod`'s Go version + add treefmt

--- flake.nix
+++ flake.nix
@@ -1,14 +1,50 @@
 {
-  description = "gostuff";
+  description = "Collection of small Go libraries used by nais K8s operators";
 
   # Flake inputs
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs"; # also valid: "nixpkgs"
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.treefmt-nix = {
+    url = "github:numtide/treefmt-nix";
+    inputs.nixpkgs.follows = "nixpkgs";
   };
 
   # Flake outputs
-  outputs = { self, nixpkgs }:
+  outputs =
+    inputs:
     let
+      goOverlay =
+        final: prev:
+        let
+          nixpkgsGo = prev.go;
+          firstSemverGeqSecond = first: second: builtins.compareVersions first second >= 0;
+          goModFileVersion = builtins.elemAt (builtins.match ".*\ngo ([0-9a\.a-z]+).*" (builtins.readFile ./go.mod)) 0;
+
+          projectGo =
+            if (firstSemverGeqSecond nixpkgsGo.version goModFileVersion) then
+              builtins.trace "Using nixpkgs' Go version: ${nixpkgsGo.version}" nixpkgsGo
+            else
+              (
+                if (firstSemverGeqSecond upstream.version nixpkgsGo.version) then
+                  builtins.warn "Using `upstream`'s Go version: ${upstream.version}, update nixpkgs?"
+                    prev.go.overrideAttrs
+                    (_: {
+                      src = prev.fetchurl rec {
+                        inherit (upstream) hash version;
+                        url = "https://go.dev/dl/go${version}.src.tar.gz";
+                      };
+                    })
+                else
+                  builtins.abort "!!! UPDATE nixpkgs or `upstream` Go's referenced version to match the one `go.mod` requires!"
+                # See below:
+              );
+
+          upstream.version = "1.23.0"; # Change us when above error is thrown
+          upstream.hash = "sha256-Qreo6A2AXaoDAi7T/eQyHUw78smQoUQWXQHu7Nb2mcY="; # Change us when above error is thrown
+        in
+        {
+          go = projectGo;
+          buildGoModule = prev.buildGoModule.override { inherit (final) go; };
+        };
       # Systems supported
       allSystems = [
         "x86_64-linux" # 64-bit Intel/AMD Linux
@@ -18,16 +54,67 @@
       ];
 
       # Helper to provide system-specific attributes
-      forAllSystems = f:
-        nixpkgs.lib.genAttrs allSystems
-        (system: f { pkgs = import nixpkgs { inherit system; }; });
-    in {
+      forAllSystems =
+        f:
+        inputs.nixpkgs.lib.genAttrs allSystems (
+          system:
+          f rec {
+            inherit (pkgs) lib;
+            pkgs = import inputs.nixpkgs {
+              inherit system;
+              overlays = [ goOverlay ];
+            };
+          }
+        );
+    in
+    {
       # Development environment output
-      devShells = forAllSystems ({ pkgs }: {
-        default = pkgs.mkShell {
-          # The Nix packages provided in the environment
-          packages = with pkgs; [ go_1_21 gotools gopls go-mockery ];
-        };
-      });
+      devShells = forAllSystems (
+        { pkgs, ... }:
+        {
+          default = pkgs.mkShell {
+            # The Nix packages provided in the environment
+            packages = with pkgs; [
+              go
+              go-mockery
+              gofumpt
+              gopls
+              gotools
+            ];
+          };
+        }
+      );
+      formatter = forAllSystems (
+        { pkgs, lib, ... }:
+        inputs.treefmt-nix.lib.mkWrapper pkgs (
+          {
+            projectRootFile = "flake.nix";
+            settings.global.excludes = [
+              "*.md"
+              ".gitattributes"
+            ];
+          }
+          // {
+            programs =
+              lib.genAttrs
+                [
+                  # General
+                  "shellcheck"
+                  "dos2unix"
+
+                  # go
+                  "gofumpt"
+
+                  # nix
+                  "statix"
+                  "nixfmt"
+                  "deadnix"
+                ]
+                (_: {
+                  enable = true;
+                });
+          }
+        )
+      );
     };
 }
