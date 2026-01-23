f904beae51520012edf419f0ecde5c40abc59469
Trong Huu Nguyen <trong.huu.nguyen@nav.no>
2026-01-23 08:56:56 +0100

    jwker: enable status subresource

--- pkg/apis/nais.io/v1/jwker_types.go
+++ pkg/apis/nais.io/v1/jwker_types.go
@@ -18,15 +18,26 @@ type JwkerSpec struct {
 
 // JwkerStatus defines the observed state of Jwker
 type JwkerStatus struct {
-	SynchronizationTime       int64  `json:"synchronizationTime,omitempty"`
-	SynchronizationState      string `json:"synchronizationState,omitempty"`
-	SynchronizationHash       string `json:"synchronizationHash,omitempty"`
-	SynchronizationSecretName string `json:"synchronizationSecretName,omitempty"`
+	// ClientID is the OAuth2 Client ID associated with this Jwker
+	ClientID string `json:"clientID,omitempty"`
+	// KeyIDs are the JWK Key IDs (kid) associated with this Jwker
+	KeyIDs []string `json:"keyIDs,omitempty"`
+	// ObservedGeneration is the most recent generation observed by the controller.
+	// If this is equal to metadata.generation, then that means that the current reported status reflects the most recent desired state of the resource.
+	ObservedGeneration        int64       `json:"observedGeneration,omitempty"`
+	SynchronizationTimestamp  metav1.Time `json:"synchronizationTimestamp,omitempty"`
+	SynchronizationState      string      `json:"synchronizationState,omitempty"`
+	SynchronizationSecretName string      `json:"synchronizationSecretName,omitempty"`
 }
 
 // +genclient
-// +kubebuilder:printcolumn:name="Secret",type="string",JSONPath=".spec.secretName"
+// +kubebuilder:printcolumn:name="Created",type="date",JSONPath=".metadata.creationTimestamp"
+// +kubebuilder:printcolumn:name="Synced",type="date",JSONPath=".status.synchronizationTimestamp"
+// +kubebuilder:printcolumn:name="State",type="string",JSONPath=".status.synchronizationState"
+// +kubebuilder:printcolumn:name="Client ID",type="string",JSONPath=".status.clientID",priority=1
+// +kubebuilder:printcolumn:name="Secret",type="string",JSONPath=".status.synchronizationSecretName",priority=1
 // +kubebuilder:object:root=true
+// +kubebuilder:subresource:status
 
 // Jwker is the Schema for the jwkers API
 type Jwker struct {
