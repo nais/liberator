ff2dd226100ba1a203168443e4b98c16e52656e9
x10an14-nav <christian.chavez@nav.no>
2026-07-16 20:32:15 +0000

    upkeep(golang): bump to 1.26.5

--- flake.nix
+++ flake.nix
@@ -1,93 +1,56 @@
 {
   description = "Collection of small Go libraries used by nais K8s operators";
 
-  # Flake inputs
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
   inputs.treefmt-nix = {
     url = "github:numtide/treefmt-nix";
     inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  # Flake outputs
   outputs =
     inputs:
-    let
-      goOverlay =
-        final: prev:
-        let
-          nixpkgsGo = prev.go;
-          firstSemverGeqSecond = first: second: builtins.compareVersions first second >= 0;
-          goModFileVersion = builtins.elemAt (builtins.match ".*\ngo ([0-9a\.a-z]+).*" (builtins.readFile ./go.mod)) 0;
-
-          projectGo =
-            if (firstSemverGeqSecond nixpkgsGo.version goModFileVersion) then
-              builtins.trace "Using nixpkgs' Go version: ${nixpkgsGo.version}" nixpkgsGo
-            else
-              (
-                if (firstSemverGeqSecond upstream.version nixpkgsGo.version) then
-                  builtins.warn "Using `upstream`'s Go version: ${upstream.version}, update nixpkgs?"
-                    prev.go.overrideAttrs
-                    (_: {
-                      src = prev.fetchurl rec {
-                        inherit (upstream) hash version;
-                        url = "https://go.dev/dl/go${version}.src.tar.gz";
-                      };
-                    })
-                else
-                  builtins.abort "!!! UPDATE nixpkgs or `upstream` Go's referenced version to match the one `go.mod` requires!"
-                # See below:
-              );
-
-          upstream.version = "1.26.4"; # Change us when above error is thrown
-          upstream.hash = ""; # Change us when above error is thrown
-        in
-        {
-          go = projectGo;
-          buildGoModule = prev.buildGoModule.override { inherit (final) go; };
+    inputs.flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import inputs.nixpkgs {
+          localSystem = { inherit system; };
+          overlays = [
+            (
+              final: prev:
+              let
+                version = "1.26.5";
+                newerGoVersion = prev.go_latest.overrideAttrs (old: {
+                  inherit version;
+                  src = prev.fetchurl {
+                    url = "https://go.dev/dl/go${version}.src.tar.gz";
+                    hash = "sha256-SVvkvIcXasVnOS5bQRar2YRm0z17SdQedkzMaXay3EI=";
+                  };
+                });
+                nixpkgsVersion = prev.go_latest.version;
+                newVersionNotInNixpkgs = -1 == builtins.compareVersions nixpkgsVersion version;
+              in
+              {
+                go_latest = if newVersionNotInNixpkgs then newerGoVersion else prev.go_latest;
+                buildGoModule = prev.buildGoModule.override { go = final.go_latest; };
+              }
+            )
+          ];
         };
-      # Systems supported
-      allSystems = [
-        "x86_64-linux" # 64-bit Intel/AMD Linux
-        "aarch64-linux" # 64-bit ARM Linux
-        "x86_64-darwin" # 64-bit Intel macOS
-        "aarch64-darwin" # 64-bit ARM macOS
-      ];
-
-      # Helper to provide system-specific attributes
-      forAllSystems =
-        f:
-        inputs.nixpkgs.lib.genAttrs allSystems (
-          system:
-          f rec {
-            inherit (pkgs) lib;
-            pkgs = import inputs.nixpkgs {
-              inherit system;
-              overlays = [ goOverlay ];
-            };
-          }
-        );
-    in
-    {
-      # Development environment output
-      devShells = forAllSystems (
-        { pkgs, ... }:
-        {
-          default = pkgs.mkShell {
-            # The Nix packages provided in the environment
-            packages = with pkgs; [
-              go
-              go-mockery
-              gofumpt
-              gopls
-              gotools
-              kustomize
-            ];
-          };
-        }
-      );
-      formatter = forAllSystems (
-        { pkgs, lib, ... }:
-        inputs.treefmt-nix.lib.mkWrapper pkgs (
+        inherit (pkgs) lib;
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            go
+            go-mockery
+            gofumpt
+            gopls
+            gotools
+            kustomize
+          ];
+        };
+        formatter = inputs.treefmt-nix.lib.mkWrapper pkgs (
           {
             projectRootFile = "flake.nix";
             settings.global.excludes = [
@@ -115,7 +78,7 @@
                   enable = true;
                 });
           }
-        )
-      );
-    };
+        );
+      }
+    );
 }
